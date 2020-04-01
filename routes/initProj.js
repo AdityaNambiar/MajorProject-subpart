99f9871e87c06e9db5dc60eb55ec329c96461d1a
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-04-02 01:32:34 +0530

    Rewrote initProj.js

--- routes/initProj.js
+++ routes/initProj.js
@@ -6,6 +6,9 @@
 // Misc:
 const addToIPFS = require('../utilities/addToIPFS');
 
+// Terminal execution:
+const { exec } = require('child_process');
+
 // isomorphic-git related imports and setup
 const fs = require('fs');
 const git = require('isomorphic-git');
@@ -21,63 +24,121 @@ let README = `PROJECT NAME: ${projName} \n PROJECT DESCRIPTION: ${projDesc} \n`
 
 
 router.post('/initProj', async (req,res) => {
-    const projLeader = "Aditya" // Hard coded - has to card name or from blockchain?
     var projName = req.body.projName; // 
     var majorHash = '';
     // Git work:
+    let authoremail = 'adi@g.c';
+    let authorname = 'Aditya';
     let buffer = req.body.filebuff || README;
     let filename = req.body.filename || "README";
-    let ac_status = true;
     let usermsg = "Initial Commit";
     try {
-        if (!fs.existsSync(path.resolve(__dirname,'..',projLeader)))
-            main(projLeader,projName,majorHash,res, buffer, filename, ac_status, usermsg)
+        if (!fs.existsSync(path.resolve(__dirname,'..','projects',projName)))
+            main(projName, majorHash, res, buffer, filename, usermsg)
     } catch (err) {
         console.log("git init main err: ", err)
     }
 })
 
-async function main(projLeader, projName, majorHash, res, buffer, filename, ac_status, usermsg) {
-    // Create project folder with provided leader's name
-    fs.mkdir(path.resolve(__dirname, '..',projLeader), async (err)=>{
-        if (err) console.log("mkdir (leader's folder) err: ",err);
-
-        // Create actual project folder with leader's 
-        fs.mkdir(path.resolve(__dirname,'..',projLeader,projName), async (err) => {
+async function main(projName, majorHash, res, buffer, filename, usermsg) {
+    fs.mkdir(path.resolve(__dirname,'..','projects',projName), async (err) => {
             if (err) console.log("mkdir (proj folder) err: ", err);
 
             // Initialize this folder as git repo 
+            await gitInit(projName)
+            .then( async () => {
+                try {
+                    await writeFile(projName,filename, buffer);
+                }catch(e) {
+                    res.status(400).send({msg: "writeFile err: "+e})
+                }
+            })
+            .then( async () => {
+                 try{
+                    await autoCommit(projName,filename, usermsg, authorname, authoremail);
+                 }catch(e){
+                    res.status(400).send({msg: "autocommit err: "+e})
+                 }
+            })
+            .then( async () => {
+                try{
+                    await clone(projName)
+                    console.log("clone done.");
+                } catch(e) {
+                    res.status(400).send({msg: "clone err: "+e})
+                }
+            })
+            .then( async () => {
+                majorHash = await addToIPFS(projName+'.git');
+                console.log("MajorHash (git init): ", majorHash);
+                res.status(200).send({projName: projName, majorHash: majorHash})
+            })
+            .catch((e) => {
+                console.log('main catch err: ',e);
+            })
+        })
+}
+
+async function gitInit(projName) {
+    return new Promise( async (resolve, reject) => {
+        try {
             await git.init({
                 fs,
-                dir: path.resolve(__dirname,'..',projLeader,projName)
+                dir: path.resolve(__dirname,'..',projName)
             });
-            fs.writeFileSync(path.join(__dirname, projLeader, projName,filename),Buffer.from(buffer));
-            try {
-                await git.add({
-                    dir:  path.join(__dirname, projLeader, projName),
-                    filepath: filename
-                })
-                console.log(`File added is -> ${filename}`);
+            resolve(true)
+        } catch(e) {
+            reject(e);
+        }
+    })
+}
 
-                if (ac_status) { // if auto-commit is true i.e. user wants to commit when they add this file...
-                    let sha = await git.commit({
-                        fs,
-                        dir:  path.join(__dirname, projLeader, projName),
-                        message: usermsg,
-                        author: {
-                            name: "Aditya",
-                            email: "adi@g.c"
-                        }
-                    })
-                    console.log("commit hash: \n",sha);
-                }
-            res.status(200).send({message:"git init done"});
-            }catch(e){
-                console.log("git init err: ",e);
-                res.status(400).send(e)
-            }
-            majorHash = await addToIPFS(projLeader,projName);
-            console.log("MajorHash (git init): ", majorHash);
+async function clone(projName) {
+    return new Promise( async (resolve, reject) => {
+        try {
+            await exec(`git clone --bare ${projName} ${projName+'.git'}`,{
+                cwd: '.',
+                shell: true
+            }, (err, stdout, stderr) => {
+                if (err) console.log('init clone cli err: ',err);
+                if (stderr) console.log('init clone cli stderr: ',stderr);
+                resolve(true)
+            })
+        } catch(e) {
+            reject(e);
+        }
+    })
+}
+
+async function autoCommit(projName, filename, usermsg, authorname, authoremail){
+    return new Promise( async (resolve, reject) => {
+        try {
+            await git.add({
+                dir:  path.join(__dirname, '..', 'projects', projName),
+                filepath: filename
+            })
+            let sha = await git.commit({
+                    fs,
+                    dir:  path.join(__dirname, '..', 'projects', projName),
+                    message: usermsg,
+                    author: {
+                        name: authorname,
+                        email: authoremail
+                    }
+                })
+            console.log("commit hash: \n",sha);
+            resolve(true);
+        } catch(e) {
+            reject(e);
+        }
+    })
+}
+
+async function writeFile(projName, filename, buffer) {
+    return new Promise( async (resolve, reject) => {
+        fs.writeFile(path.resolve('projects',projName, filename),Buffer.from(buffer),(err) => {
+            if (err) { console.log('fs write err: ', err); reject(e); }
+            resolve(true);
         })
     })
 }

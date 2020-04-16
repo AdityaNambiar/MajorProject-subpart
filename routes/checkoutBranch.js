605c01b9e333ce3eb69355d0ac3b43a2296515e9
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-04-16 20:08:45 +0530

    Brought back checkoutBranch

---
+++ routes/checkoutBranch.js
@@ -0,0 +1,138 @@
+
+/**
+ * Get (Read) list of files in current branch of git repository.
+ * (No changes in .git/ folder - confirmed)
+ */
+
+// Misc:
+const addToIPFS = require('../utilities/addToIPFS');
+const preRouteChecks = require('../utilities/preRouteChecks');
+const removeFromIPFS = require('../utilities/removeFromIPFS');
+const statusChecker = require('../utilities/statusChecker');
+const pushChecker = require('../utilities/pushChecker');
+const pushToBare = require('../utilities/pushToBare');
+const rmWorkdir = require('../utilities/rmWorkdir');
+
+// Terminal execution import
+const { exec } = require('child_process');
+
+// isomorphic-git related imports and setup
+const fs = require('fs');
+const git = require('isomorphic-git');
+git.plugins.set('fs',fs); // Bring your own file system 
+
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+var projName, workdirpath, curr_majorHash, 
+    username, branchToUpdate, majorHash, 
+    barerepopath, filenamearr, statusLine,
+    upstream_branch;
+
+router.post('/checkoutBranch', async (req,res) => {
+    projName = req.body.projName.replace(/\s/g,'-');
+    branchToUpdate = req.body.branchToUpdate.replace(/\s/g,'-');
+    username = req.body.username.replace(/\s/g,'-');
+    curr_majorHash = req.body.majorHash; // hard coded
+    upstream_branch = 'origin/master';
+
+    barerepopath = path.resolve(__dirname, '..', 'projects', 'bare', projName+'.git'); 
+    workdirpath = path.resolve(__dirname, '..', 'projects', projName, username);
+
+    try{
+        await preRouteChecks(curr_majorHash, projName, username, branchToUpdate)
+        .then( async () => {
+            let response = await main(projName, curr_majorHash)
+            return response;
+        })
+        .then ( (response) => {
+            res.status(200).send(response);
+        })
+    }catch(e){
+        res.status(400).send(`main caller err: ${e}`);
+    }
+})
+
+async function main(projName, curr_majorHash){
+    return new Promise ( async (resolve, reject) => {
+        try {
+            await gitCheckout(workdirpath)
+            .then( async () => {
+                await setUpstream(workdirpath, upstream_branch);
+            })
+            .then ( async () => {
+                statusLine = await statusChecker(projName, username);
+                return statusLine;
+            })
+            .then( async () => {
+                filenamearr = [];
+                filenamearr = await pushChecker(projName, username, branchToUpdate); 
+                console.log("pushchecker returned this: \n", filenamearr);
+            })
+            if (filenamearr.length == 0) {  // if no conflicts only then proceed with cleaning up.
+                console.log(`Pushing to branch: ${branchToUpdate}`);
+                await pushToBare(projName, branchToUpdate, username)
+                .then( async () => {
+                    await rmWorkdir(projName, username);
+                })
+                .then( async () => {
+                    // Remove old state from IPFS.
+                    await removeFromIPFS(curr_majorHash, projName);
+                })
+                .then( async () => {
+                    // Add new state to IPFS.
+                    majorHash = await addToIPFS(barerepopath);
+                    return majorHash;
+                })
+                .then( (majorHash) => {
+                    console.log("MajorHash (git getFiles): ", majorHash);
+                    resolve({projName: projName, majorHash: majorHash, filenamearr: filenamearr, statusLine: statusLine});
+                })
+            } else if (filenamearr[0] != "Please solve this merge conflict via CLI"){
+                resolve({projName: projName, majorHash: majorHash, filenamearr: filenamearr, statusLine: statusLine});
+            } else {
+                resolve({projName: projName, majorHash: curr_majorHash, filenamearr: filenamearr, statusLine: statusLine});
+            }
+        } catch (e) {
+            reject(`main err: ${e}`);
+        }
+    })
+}
+
+async function gitCheckout(workdirpath){
+    return new Promise (async (resolve, reject) => {
+        try {
+            exec(`git checkout ${branchToUpdate}`, {
+                cwd: workdirpath,
+                shell: true
+            }, (err,stdout,stderr) => {
+                if(err) { console.log('err: '+err); reject(`git-checkout cli err: ${err}`);}
+                //if(stderr) {console.log('stderr: '+stderr);reject(`git-checkout cli stderr: ${stderr}`);}
+                resolve(true);
+            })
+        }catch(e){
+            reject(`git-checkout err: ${e}`);
+        }
+    })
+}
+
+async function setUpstream(workdirpath, upstream_branch) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            exec(`git branch --set-upstream-to=${upstream_branch}`, {
+                cwd: workdirpath,
+                shell: true
+            }, (err, stdout, stderr) => {
+                if (err) reject(`git-setupstream err: ${err}`);
+                if (stderr) reject(`git-setupstream stderr: ${stderr}`);
+                console.log(stdout);
+            })
+            resolve(true);
+        } catch(e) {
+            reject(`git-branch-setUpstream err: ${e}`)
+        } 
+    })
+}
+
+module.exports = router;

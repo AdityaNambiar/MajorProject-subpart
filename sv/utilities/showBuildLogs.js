6d5922c9826f452e7e63a41fe5f6bc9d2f416289
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-05-17 19:05:05 +0530

    Updated showBuildLogs

--- sv/utilities/showBuildLogs.js
+++ sv/utilities/showBuildLogs.js
@@ -1,9 +1,9 @@
 
 const jenkinslogsapi = require('jenkins')({ baseUrl: 'http://admin:11a4469a856bdf30c30a7c0053f822beaa@localhost:8080', crumbIssuer: true }); // Naming this specially like this because I am only using this package for getting logStream.
  
-const fs = require('fs');
+//const fs = require('fs');
 
-module.exports = function showLogs(jenkins, jenkinslogsapi, projName){
+module.exports = function showLogs(projName, buildnumber){
     return new Promise( (resolve, reject) => {
         try {
             //let buildnumber = fs.readFileSync(projName+'-'+'currjob_buildno.txt', 'utf8');
